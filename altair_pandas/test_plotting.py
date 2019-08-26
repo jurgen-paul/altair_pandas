7044b78fe6766ebfae7d8f364a4bd4bcc3d3a2c0
Philipp Kats <casyfill@gmail.com>
2019-08-26 18:53:37 -0400

    removed parametrized testing

--- altair_pandas/test_plotting.py
+++ altair_pandas/test_plotting.py
@@ -13,81 +13,6 @@ def dataframe():
     return pd.DataFrame({'x': range(5), 'y': range(5)})
 
 
-<<<<<<< HEAD
-series_cases = {
-    'line': {
-        'answer': {
-            'mark': 'line',
-            'encoding_x': 'index',
-            'encoding_y': 'data_name'
-        }
-    },
-
-    'bar': {
-        'answer': {
-            'mark': 'bar',
-            'encoding_x': 'index',
-            'encoding_y': 'data_name'
-        }
-    },
-
-    'area': {
-        'answer': {
-            'mark': 'area',
-            'encoding_x': 'index',
-            'encoding_y': 'data_name'
-        }
-    }
-}
-
-@pytest.mark.parametrize('kind, props', series_cases.items())
-def test_series_plot(series, with_plotting_backend, kind, props):
-    chart = series.plot(kind=kind)
-    spec = chart.to_dict()
-
-    answer = props['answer']
-
-    assert spec['mark'] == answer['mark']
-    assert spec['encoding']['x']['field'] == answer['encoding_x']
-    assert spec['encoding']['y']['field'] == answer['encoding_y']
-
-
-dataframe_cases = {
-    'line': {
-        'answer': {
-            'mark': 'line',
-            'encoding_x': 'index',
-            'encoding_y': 'value'
-        }
-    },
-
-    'bar': {
-        'answer': {
-            'mark': 'bar',
-            'encoding_x': 'index',
-            'encoding_y': 'value'
-        }
-    },
-
-    'area': {
-        'answer': {
-            'mark': 'area',
-            'encoding_x': 'index',
-            'encoding_y': 'value'
-        }
-    }
-}
-
-@pytest.mark.parametrize('kind, props', dataframe_cases.items())
-def test_dataframe_plot(dataframe, with_plotting_backend, kind, props):
-    chart = dataframe.plot(kind=kind)
-    spec = chart.to_dict()
-
-    answer = props['answer']
-    assert spec['mark'] == answer['mark']
-    assert spec['encoding']['x']['field'] == answer['encoding_x']
-    assert spec['encoding']['y']['field'] == answer['encoding_y']
-=======
 @pytest.mark.parametrize('data', [
     pd.Series(
         range(6),
@@ -144,7 +69,6 @@ def test_dataframe_basic_plot(dataframe, kind, with_plotting_backend):
         assert spec['mark'] == kind
     assert spec['encoding']['x']['field'] == 'index'
     assert spec['encoding']['y']['field'] == 'value'
->>>>>>> 3bdd730433590a70683ca83480e0f9ca4db75fd2
     assert spec['encoding']['color']['field'] == 'column'
     assert spec['transform'][0]['fold'] == ['x', 'y']
 
