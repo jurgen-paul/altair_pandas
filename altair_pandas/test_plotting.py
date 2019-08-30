db78d9854b478eb66a7e8fbd219d30b601cd9dc8
Pratap Vardhan <pratap.vardhan@gramener.com>
2019-08-30 11:06:28 +0530

    ENH: Expose stacked from self._xy() helper

--- altair_pandas/test_plotting.py
+++ altair_pandas/test_plotting.py
@@ -211,17 +211,18 @@ def test_dataframe_mark_properties(dataframe, kind, with_plotting_backend):
 def test_series_mark_properties(series, kind, with_plotting_backend):
     chart = series.plot(kind=kind, alpha=0.5, color="red")
     spec = chart.to_dict()
-    assert spec['mark']['type'] == _expected_mark(kind)
-    assert spec['mark']['opacity'] == 0.5
-    assert spec['mark']['color'] == 'red'
+    assert spec["mark"]["type"] == _expected_mark(kind)
+    assert spec["mark"]["opacity"] == 0.5
+    assert spec["mark"]["color"] == "red"
 
 
 @pytest.mark.parametrize("stacked", [True, False])
 def test_dataframe_area(dataframe, stacked, with_plotting_backend):
     chart = dataframe.plot.area(stacked=stacked)
     spec = chart.to_dict()
-    mark = {"type": "area"} if stacked else {
-        "type": "area", "line": True, "opacity": 0.5}
+    mark = (
+        {"type": "area"} if stacked else {"type": "area", "line": True, "opacity": 0.5}
+    )
     assert spec["mark"] == mark
     for k, v in {"x": "index", "y": "value", "color": "column"}.items():
         assert spec["encoding"][k]["field"] == v
